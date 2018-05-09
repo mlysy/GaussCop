96057cd84a27c589322c8c10af395214c4751a6a
mlysy <mlysy@users.noreply.github.com>
2018-05-09 14:53:09 -0400

    modified vignette

--- R/gcopSub.R
+++ R/gcopSub.R
@@ -4,20 +4,24 @@
 #' @param subset Integer or logical vector specifying which subset of variables to keep. Default is to keep all variables.
 #' @return A \code{gaussCop} object representing the marginal Gaussian Copula distribution on the subset of variables.
 #' @examples
-#' # simulate data and plot it
+#' # simulate data
 #' n <- 5e4
-#' dat <- cbind(rnorm(n, mean = 1, sd = 3),
-#'              rnorm(n, mean=4, sd = 0.5),
-#'              rt(n, df = 5),
-#'              rchisq(n, df = 3),
-#'              rnorm(n, mean=10, sd = 10))
-#' plot(dat[, c(3, 4)]) # plot a subset of the data
+#' X <- cbind(rnorm(n, mean = 1, sd = 3),
+#'            rnorm(n, mean=4, sd = 0.5),
+#'            rt(n, df = 10),
+#'            rchisq(n, df = 5),
+#'            rnorm(n, mean=10, sd = 10))
 #' # fit Gaussian Copula using Kernel method
-#' temp.cop <- gcopFit(X = dat, fitXD = "gc4")
-#' sub.cop <- gcopSub(temp.cop, c(3, 4)) # subset copula
-#' # simulate data from Copula model and add it to plot, should blend in
-#' new.data <- rgcop(100, sub.cop)
-#' points(new.data, cex = 0.5, col="red") # plot points from subsetted copula
+#' gCop <- gcopFit(X, fitXD = "kernel")
+#' # subset gCop
+#' isub <- sample(5, 3) # subset
+#' gCop.sub <- gcopSub(gCop, subset = isub)
+#' # check that subsetted xDensity representations are the same
+#' for(ii in 1:3) {
+#'   print(identical(gCop$XDens[[isub[ii]]], gCop.sub$XDens[[ii]]))
+#' }
+#' # check that subsetted correlation matrix is the same
+#' print(identical(gCop$Rho[isub,isub], gCop.sub$Rho))
 #' @export
 gcopSub <- function(gCop, subset) {
   # convert subset to a numeric vector
